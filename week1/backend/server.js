e0e5ad9511832e1c85d0b321f3c4769958e32c54
abhilasha jangid <abhilashajangid786@gmail.com>
2017-12-04 23:05:20 -0800

    week1 backend task1 completed

---
+++ week1/backend/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var app = express();
+
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.get('/' , function(req,res)
+{
+    res.send('hello world- abhilasha');
+})
+
+
+
+app.listen(8080,function()
+{
+    console.log('server listen on port 8080');
+})
