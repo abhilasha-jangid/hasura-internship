67ddaf5bf23958d5c4dba4323b8a69f6dd9fa2ca
abhilasha jangid <abhilashajangid786@gmail.com>
2017-12-05 06:24:26 -0800

    week1 backend task2 part (a) completed

--- week1/backend/server.js
+++ week1/backend/server.js
@@ -2,7 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var app = express();
-
+const https = require('https');
+const options = {
+  hostname: 'jsonplaceholder.typicode.com',
+  port: 443,
+  path: '/users',
+  method: 'GET'
+};
 
 app.use(morgan('dev'));
 
@@ -13,8 +19,40 @@ app.get('/' , function(req,res)
 {
     res.send('hello world- abhilasha');
 })
-
-
+app.get('/users',function(req,res)
+{
+   r = https.request(options ,function(resp){
+    output = ''
+    username=[]
+    console.log('STATUS:',resp.statusCode);
+    resp.on('data',function(chunk)
+    {
+      console.log('a new chunk',chunk)
+      output += chunk
+    });
+    resp.on('end',function()
+  {
+    var users = JSON.parse(output);
+    for(var i=0;i<users.length;i++)
+    {
+      username.push(users[i]["name"]);
+    }
+    res.json(
+      {
+         "message" :
+         {
+           "data" : username
+         }
+    });
+    console.log('END GET REQUEST')
+  });
+  })
+  r.on('error',function(err)
+  {
+    console.log('Error: ',err)
+  })
+  r.end()
+})
 
 app.listen(8080,function()
 {
