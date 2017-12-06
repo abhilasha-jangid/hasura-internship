b95dcae5d13f1c073039d521a734b45c64d6faf7
abhilasha jangid <abhilashajangid786@gmail.com>
2017-12-06 10:56:24 -0800

    week1 backend task2 part (c) completed

--- week1/backend/routes/posts.js
+++ week1/backend/routes/posts.js
@@ -14,73 +14,73 @@ const option = {
   path: '/users',
   method: 'GET'
 };
+postRoutes.get('/counter',function(req,res)
+{
+  authorname = ''
+  posts = ''
+  r = https.request(option ,function(resp)
+  {
+        console.log('STATUS:', +resp.statusCode);
+        resp.on('data',function(chunk)
+        {
+        //  console.log('a new chunk',chunk)
+          authorname += chunk
+        });
+        resp.on('end',function()
+      {
+        console.log('END GET REQUEST')
+      });
+  })
+  r.on('error',function(err)
+  {
+        console.log('Error: ',err)
+  })
+  r.end()
 
-postRoutes.get('/users',function(req,res)
-{
-       r = https.request(option ,function(resp){
-        output = ''
-        username=[]
-        console.log('STATUS:',resp.statusCode);
-        resp.on('data',function(chunk)
+  p = https.request(options ,function(resp){
+    console.log('STATUS:', +resp.statusCode);
+    resp.on('data',function(chunk)
+    {
+          //console.log('a new chunk',chunk)
+      posts += chunk
+    });
+    resp.on('end',function()
+    {
+        var dict = []
+        authorname = JSON.parse(authorname);
+        posts = JSON.parse(posts);
+        //console.log('STATUS:',resp.statusCode);
+        for(var i=0;i<authorname.length;i++)
         {
-          console.log('a new chunk',chunk)
-          output += chunk
-        });
-        resp.on('end',function()
-      {
-        var users = JSON.parse(output);
-        for(var i=0;i<users.length;i++)
-        {
-          username.push(users[i]["name"]);
+          var name = authorname[i]["name"]
+          var id = authorname[i]["id"]
+          var count = 0
+          for(var j=0;j<posts.length;j++)
+          {
+            if(posts[j]["userId"] == id)
+            {
+              count++;
+            }
+          }
+          dict.push({
+                        key:   name,
+                        value: count
+                    });
         }
         res.json(
-          {
-             "message" :
-             {
-               "data" : username
-             }
-        });
-        console.log('END GET REQUEST')
-      });
+            {
+               "message" :
+               {
+                 "data" :  dict
+               }
+          })
       })
-      r.on('error',function(err)
+      console.log('END GET REQUEST')
+      })
+      p.on('error',function(err)
       {
         console.log('Error: ',err)
       })
-      r.end()
-})
-postRoutes.get('/posts',function(req,res)
-{
-       r = https.request(options ,function(resp){
-        output = ''
-        postdis=[]
-        console.log('STATUS:',resp.statusCode);
-        resp.on('data',function(chunk)
-        {
-          console.log('a new chunk',chunk)
-          output += chunk
-        });
-        resp.on('end',function()
-      {
-        var posts = JSON.parse(output);
-        for(var i=0;i<posts.length;i++)
-        {
-          postdis.push(posts[i]["body"]);
-        }
-        res.json(
-          {
-             "message" :
-             {
-               "data" :  postdis
-             }
-        });
-        console.log('END GET REQUEST')
-      });
-      })
-      r.on('error',function(err)
-      {
-        console.log('Error: ',err)
-      })
-      r.end()
-})
+      p.end()
+    });
 module.exports = postRoutes;
